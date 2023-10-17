5a49615e7fe968e74024eaf4235a0b80b53eca43
Anali2128 <142955067+Anali2128@users.noreply.github.com>
2023-10-16 23:01:10 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,91 @@
+localStorage.setItem('cart', JSON.stringify([]))
+
+const cart = []
+
+/* verificamos si existe un carrito en localstorage */
+const cartFromLocalStorage = localStorage.getItem('cart')
+
+if(cartFromLocalStorage){
+    /* si existe voy a guardar cada item en mi cart */
+    const carrtitoFromLocalStorage = JSON.parse(localStorage.getItem('cart'))
+    for(let item of carritoFromLocalStorage){
+        cart.push(item)
+    } 
+}
+localStorage.setItem('cart', JSON.stringify([]))
+
+
+const productos = [
+{
+    nombre: 'atrapasueno Ama',
+    precio: 2400,
+    id: 3
+}
+]   
+
+
+
+let direccionDeBusqueda = window.location.search
+
+
+const urlSearchParams = new urlSearchParams(window.location.search)
+
+const productId = urlSearchParams. get(productId)
+const productHtml = document.getElementById('products')
+
+
+
+/* buscar un producto por Id */
+
+const productFound = products.find(product => product.id == productId)
+
+const resultHTML = document.getElementById('result')
+let controls = `<button id= 'btn-comprar'>comprar</button>`
+const renderProductDetail =() => {
+
+}
+
+if(cart.some(product => product.id == productFound.id)){
+    controls = 
+        `<button id = 'btn-comprar'>comprar</button>
+}`
+/* si ya esta comprado el producto */
+if(cart.some(product => productId == productFound.id)){
+    controls = `<div>
+    <button id eliminar producto> - </button>
+    <span> 1 </span>
+    <button id agregar producto> + </button>
+    </div>`
+}
+}
+
+
+resultHTML.innerHTML = `
+    <h2>${productFound.nombre}</h2>
+    <div>Precio: $${productFound.precio}</div>
+    <div>${productFound.categoria}</div>
+    <p>${productFound.description}</p>
+    ${controls}
+`
+
+
+const btnComprar = document.getElementById('btn-comprar')
+
+btnComprar.addEventListener('click', () =>{
+    /* accion de darle click al boton comprar */
+    cart.push(productFound)
+    localStorage.setItem('cart', JSON.stringify(cart))
+})
+
+const elementoCarrito = document.querySelector('.productos')
+
+elementoCarrito.innerText = cart.join(', ')
+
+function agregarProducto() {
+cart.push('producto')
+localStorage.setItem('cart', JSON.stringify(cart))
+}
+
+const botonAgregarProducto = document.querySelector('.boton-agregar')
+
+botonAgregarProducto.addEventListener('click', agregarProducto)
--- script6.js
+++ script6.js
@@ -14,56 +14,15 @@ if(cartFromLocalStorage){
 }
 localStorage.setItem('cart', JSON.stringify([]))
 
+
 const productos = [
-    {
-        nombre: 'Manta Silla',
-        precio: 3600,
-        tamano: '40 x 40',
-        id: 0
-    },
-    {
-        nombre: 'Manta Cama',
-        precio: 4000,
-        tamano: '80 x 80',
-        id: 1
-    },   
-    {   
-        nombre: 'Manta sofa',
-        precio: 3000,
-        tamano: '20 x 20',
-        id: 2
-    },
-    {
-        nombre: 'atrapasueno Ama',
-        precio: 2400,
-        id: 3
-    },
-    {
-        nombre: 'atrapasueno Vive',
-        precio: 2400,
-        id: 4
-    },
-    {
-        nombre: 'atrapasueno Espera',
-        precio: 2800,
-        id: 5
-    },
-    {
-        nombre: 'atrapasueno Arcoiris Rosa',
-        precio: 2600,
-        id: 6
-    },
-    {
-        nombre: 'Corazones',
-        precio: 1800,
-        id: 7
-    },
-    {
-        nombre: 'atrapasueno Love',
-        precio: 2800,
-        id: 8
-    }
-]
+{
+    nombre: 'Corazones',
+    precio: 1800,
+    id: 7
+}
+]   
+
 
 
 let direccionDeBusqueda = window.location.search
